d46ee4e1c73a6dd1f68bfa27041208c6cd87d90d
Marco Albarelli <marco.albarelli@iks.it>
2015-07-14 15:30:50 +0200

    Added access control to the admin area

--- src/Marcoalbarelli/AdminBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Marcoalbarelli/AdminBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,18 +3,64 @@
 namespace Marcoalbarelli\AdminBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    private $client;
+
+    /**
+     * @dataProvider adminRouteProvider
+     * @param $route
+     * @param $parameters
+     */
+    public function testAllAdminRoutesAreUnacessibleByAnonymousUsers($route,$parameters)
     {
-        $client = static::createClient();
+        $this->client = static::createClient();
 
-        $router =$client->getContainer()->get('router');
-        $route = $router->generate('admin_hello',array('name'=>"Fabien"));
+        $router =$this->client->getContainer()->get('router');
+        $route = $router->generate($route,$parameters);
 
-        $crawler = $client->request('GET', $route);
+        $this->client->request('GET', $route);
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertEquals(302,$this->client->getResponse()->getStatusCode());
+        $this->assertcontains('login',$this->client->getResponse()->headers->get('Location'));
+
+    }
+
+    /**
+     * @dataProvider adminRouteProvider
+     * @param $route
+     * @param $parameters
+     */
+    public function testAdminUsersCanAccessAdminRoutes($route,$parameters){
+        $this->client = static::createClient();
+        $this->logIn();
+        $router = $this->client->getContainer()->get('router');
+        $route = $router->generate($route,$parameters);
+
+        $this->client->request('GET', $route);
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function adminRouteProvider(){
+        $out = [];
+        $out[] = array('admin_hello',array('name'=>"Fabien"));
+        return $out;
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'admin_area';
+        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
     }
 }
