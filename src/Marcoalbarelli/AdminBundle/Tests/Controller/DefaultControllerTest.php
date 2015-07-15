df28af715b765007c68870144855cf7c003689fe
Marco Albarelli <marco.albarelli@iks.it>
2015-07-15 11:58:34 +0200

    Implemented the basic_auth part of #7 added the .htaccess to gitignore

--- src/Marcoalbarelli/AdminBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Marcoalbarelli/AdminBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,11 +3,17 @@
 namespace Marcoalbarelli\AdminBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Client;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
+    const ADMIN_AREA_FIREWALL_NAME = 'admin_area';
+
+    /**
+     * @var $client Client
+     */
     private $client;
 
     /**
@@ -36,7 +42,8 @@ class DefaultControllerTest extends WebTestCase
      */
     public function testAdminUsersCanAccessAdminRoutes($route,$parameters){
         $this->client = static::createClient();
-        $this->logIn();
+
+        $this->logInAdmin();
         $router = $this->client->getContainer()->get('router');
         $route = $router->generate($route,$parameters);
 
@@ -45,19 +52,46 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
+    /**
+     * @dataProvider adminRouteProvider
+     * @param $route
+     * @param $parameters
+     */
+    public function testNonAdminUsersAreBlocked($route,$parameters){
+        $this->client = static::createClient();
+        $router = $this->client->getContainer()->get('router');
+        $route = $router->generate($route,$parameters);
+
+        //Login via SessionCookie
+        $this->logInUser();
+        $this->client->request('GET', $route);
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+        //Login via http_basic (just to check that a useful test approach doesn't break
+        $this->client = static::createClient(); //refresh the client to drop the session
+        $this->client->request('GET', $route,array(),array(),array('PHP_AUTH_USER'=>'foo','PHP_AUTH_PW'=>'foo'));
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+    }
+
     public function adminRouteProvider(){
         $out = [];
         $out[] = array('admin_hello',array('name'=>"Fabien"));
         return $out;
     }
 
-    private function logIn()
+    private function loginAdmin(){
+        $this->logIn('admin','ROLE_ADMIN');
+    }
+
+    private function loginUser(){
+        $this->logIn('user','ROLE_USER');
+    }
+
+    private function logIn($username,$role)
     {
         $session = $this->client->getContainer()->get('session');
 
-        $firewall = 'admin_area';
-        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
-        $session->set('_security_'.$firewall, serialize($token));
+        $token = new UsernamePasswordToken($username, null, self::ADMIN_AREA_FIREWALL_NAME, array($role));
+        $session->set('_security_'.self::ADMIN_AREA_FIREWALL_NAME, serialize($token));
         $session->save();
 
         $cookie = new Cookie($session->getName(), $session->getId());
